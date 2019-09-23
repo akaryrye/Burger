eec4bb65642dc5531bfab0803bcfc66911dd8532
akaryrye <ralldrin@gmail.com>
2019-09-23 00:00:31 -0700

    Added styling with materialize

--- public/assets/js/ajax.js
+++ public/assets/js/ajax.js
@@ -1,55 +1,66 @@
-$("#submit").on("click", function (e) {
-    e.preventDefault();
-    var newBurger = {
-        name: $("#name").val().trim(),
-        description: $("#description").val().trim()
-    };
+$(document).ready(function(){
 
-    $.post("/api/create", newBurger)
-     .then( function (data) {
-        console.log(data);
-        $("#name").val("");
-        $("#description").val("");
-        location.reload();
+    $("#submit").on("click", function (e) {
+        e.preventDefault();
+        var newBurger = {
+            name: $("#name").val().trim(),
+            description: $("#description").val().trim()
+        };
+
+        $.post("/api/create", newBurger)
+        .then( function (data) {
+            console.log(data);
+            $("#name").val("");
+            $("#description").val("");
+            location.reload();
+        });
     });
-});
 
-$(".devour-burger").on("click", function(e) {
-    e.preventDefault();
-    var id = $(this).data("id");
-    var newState = {devour:true};
+    $(".devour-burger").on("click", function(e) {
+        e.preventDefault();
+        var id = $(this).data("id");
+        var newState = {devour:true};
 
-    $.ajax(`/api/devour/${id}`, {
-        type: "PUT",
-        data: newState
-    }).then( function () {
-        console.log("Burger Devoured");
-        location.reload();
+        $.ajax(`/api/devour/${id}`, {
+            type: "PUT",
+            data: newState
+        }).then( function () {
+            console.log("Burger Devoured");
+            location.reload();
+        });
     });
-});
 
-$(".update-burger").on("click", function(e) {
-    e.preventDefault();
-    var input = $("#update" + $(this).data("id"));
-    var data = {
-        id: $(this).data("id"),
-        description: input.val().trim()
-    }
-    console.log(data.description);
+    $(".update-burger").on("click", function(e) {
+        e.preventDefault();
+        var input = $("#update" + $(this).data("id"));
+        var data = {
+            id: $(this).data("id"),
+            description: input.val().trim()
+        }
+        console.log(data.description);
 
-    $.post("/api/update/", data)
-    .then(function(data) {
-        console.log(data);
-        location.reload();
+        $.post("/api/update/", data)
+        .then(function(data) {
+            console.log(data);
+            location.reload();
+        });
     });
-});
 
-$(".delete-burger").on("click", function(e) {
-    e.preventDefault();
-    var id = $(this).data("id");
-    $.ajax(`/api/delete-burger/${id}`, {
-        type: "DELETE"
-    }).then( function() {
-        location.reload();
+    $(".delete-burger").on("click", function(e) {
+        e.preventDefault();
+        var id = $(this).data("id");
+        $.ajax(`/api/delete-burger/${id}`, {
+            type: "DELETE"
+        }).then( function() {
+            location.reload();
+        });
     });
+
+    // initialize modal
+    var modalForm = M.Modal.init($(".modal"), {
+        opacity: 0.7,
+        inDuration: 500,
+        outDuration: 500
+    });
+
 });
